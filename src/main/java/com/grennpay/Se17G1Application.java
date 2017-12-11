983fffd1ab0e815dd19ef4d2f05e216f0b8629e5
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2017-12-11 20:10:15 +0900

    first commit

---
+++ src/main/java/com/grennpay/Se17G1Application.java
@@ -0,0 +1,12 @@
+package com.grennpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se17G1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se17G1Application.class, args);
+	}
+}
---
+++ src/test/java/com/grennpay/Se17G1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grennpay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Se17G1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
