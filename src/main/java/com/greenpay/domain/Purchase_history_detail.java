0147323658f51de146c319f8f7a4eb5d3620a52e
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2017-12-13 09:17:42 +0900

    Purchase_history_detaiの定義

---
+++ src/main/java/com/greenpay/domain/Purchase_history_detail.java
@@ -0,0 +1,29 @@
+package com.greenpay.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="purchase_history_details")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Purchase_history_detail {
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	@Column(name="purchase_id",nullable=false)
+	private int purchase_id;
+	@Column(name="product_id",nullable=false)
+	private int product_id;
+	@Column(name="quanity",nullable=false)
+	private int quanity;
+}
---
+++ src/main/java/com/greenpay/repository/Purchase_history_detailRepository.java
@@ -0,0 +1,9 @@
+package com.greenpay.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greenpay.domain.Purchase_history_detail;
+
+public interface Purchase_history_detailRepository extends JpaRepository<Purchase_history_detail, Integer>{
+
+}
