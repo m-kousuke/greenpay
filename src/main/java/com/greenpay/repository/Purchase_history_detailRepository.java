f524ebe063a614863c677abff85b39b164073c97
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2017-12-14 00:15:54 +0900

    クラス名修正(#23)

--- src/main/java/com/greenpay/repository/Purchase_history_detailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.greenpay.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.greenpay.domain.Purchase_history_detail;
-
-public interface Purchase_history_detailRepository extends JpaRepository<Purchase_history_detail, Integer>{
-
-}
