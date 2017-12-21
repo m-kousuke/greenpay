eaa751748810ce9e1e267e4f0bf6fae9a3aada60
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2017-12-21 11:59:38 +0900

    PurchaseHistoryの再追加 (#26)

---
+++ src/main/java/com/greenpay/domain/PurchaseHistory.java
@@ -0,0 +1,39 @@
+package com.greenpay.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "purchase_history")
+public class PurchaseHistory {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id", nullable = false)
+	private int id;
+
+	@Column(name = "money_id", nullable = false)
+	private String moneyId;
+
+	@Column(name = "store_id", nullable = false)
+	private String storeId;
+
+	@Column(name = "amount", nullable = false)
+	private BigDecimal amount;
+
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+}
