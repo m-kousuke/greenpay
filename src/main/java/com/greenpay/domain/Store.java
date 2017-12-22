06a3a3b4a58e130424df507725cc1f7cc11a5bda
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2017-12-22 13:35:35 +0900

    .gitignoreを反映(#44)

---
+++ src/main/java/com/greenpay/domain/Store.java
@@ -0,0 +1,34 @@
+package com.greenpay.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="stores")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Store {
+	@Id
+	@GeneratedValue
+	@Column(name="id",nullable=false)
+	private String id;
+	@Column(name="name",nullable=false)
+	private String name;
+	@Column(name="password",nullable=false)
+	private String password;
+
+	@Column(name="activated",nullable=false)
+	private int activated;
+	@Column(name="created_at",nullable=false)
+	private String createdAt;
+	@Column(name="updated_at",nullable=false)
+	private String updatedAt;
+}
