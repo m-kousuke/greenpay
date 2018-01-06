592515ae9c04a49a1d6aff553fb77441c3b873eb
kousuke <kousuke@157.105.54.26>
2018-01-06 19:27:39 +0900

    htmlの作成(#73)

--- src/main/java/com/greenpay/web/UserFinishForm.java
+++ src/main/java/com/greenpay/web/UserFinishForm.java
@@ -1,6 +1,8 @@
 package com.greenpay.web;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -9,7 +11,11 @@ public class UserFinishForm {
 	@NotNull
 	private String userId;
 	@NotNull
+	@Size(min=9, max=9)
+	@Pattern(regexp="^([a-zA-Z0-9]{8,16})$",message="図書館利用番号は半角英数字9文字で入力してください")
 	private String id;
 	@NotNull
+	@Size(min=8,max=16)
+	@Pattern(regexp="^([a-zA-Z0-9]{8,16})$",message="パスワードは半角英数字8~16文字で入力してください")
 	private String password;
 }
