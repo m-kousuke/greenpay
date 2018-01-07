d4376962627276f49d763ff6d243a46c5dfb1f66
kousuke <kousuke@192.168.1.4>
2018-01-07 15:12:18 +0900

    9文字に変更(#73)

--- src/main/java/com/greenpay/web/UserFinishForm.java
+++ src/main/java/com/greenpay/web/UserFinishForm.java
@@ -12,7 +12,7 @@ public class UserFinishForm {
 	private String userId;
 	@NotNull
 	@Size(min=9, max=9)
-	@Pattern(regexp="^([a-zA-Z0-9]{8,16})$",message="図書館利用番号は半角英数字9文字で入力してください")
+	@Pattern(regexp="^([a-zA-Z0-9]{9})$",message="図書館利用番号は半角英数字9文字で入力してください")
 	private String id;
 	@NotNull
 	@Size(min=8,max=16)
