fc66daa7d85f63b5be5f909250f0eff5305ed048
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2018-01-22 20:28:41 +0900

    正規表現の修正(#103)

--- src/main/java/com/greenpay/web/CategoryForm.java
+++ src/main/java/com/greenpay/web/CategoryForm.java
@@ -10,6 +10,6 @@ import lombok.Data;
 public class CategoryForm {
 	@NotNull(message = "必須項目です")
 	@Size(min = 1, max = 64, message ="1~64文字で入力してください")
-	@Pattern(regexp="^([a-zA-Z0-9]{8,16})$",message="使用できない文字が使われています")
+	@Pattern(regexp="^([a-zA-Z0-9])$",message="使用できない文字が使われています")
 	private String name;
 }
--- src/main/java/com/greenpay/web/ProductForm.java
+++ src/main/java/com/greenpay/web/ProductForm.java
@@ -16,7 +16,7 @@ import lombok.Data;
 public class ProductForm {
 	@NotNull(message="必須項目です")
 	@Size(min = 1, max = 64, message="1~64文字で入力してください")
-	@Pattern(regexp="^([a-zA-Z0-9]{8,16})$",message="使用できない文字が使われています")
+	@Pattern(regexp="^([a-zA-Z0-9])$",message="使用できない文字が使われています")
 	private String name;
 
 	@NotNull(message="必須項目です")
