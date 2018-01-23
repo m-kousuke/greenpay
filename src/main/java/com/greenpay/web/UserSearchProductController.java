f15a57603ec8ff2e1d4b605c8f6cf8f4be5f0958
Yamada Yuki <e155417k@st.u-gakugei.ac.jp>
2018-01-23 23:38:27 +0900

    不要なインポートの削除

--- src/main/java/com/greenpay/web/UserSearchProductController.java
+++ src/main/java/com/greenpay/web/UserSearchProductController.java
@@ -2,13 +2,9 @@ package com.greenpay.web;
 
 import java.util.List;
 
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
